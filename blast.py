8d0e5d13f81db5aace5aac2b80fbe8d04eabeae0
Ferran <ferran.pegenaute@upf.edu>
2021-11-08 15:43:43 +0100

    BLAST hit retrieval and runner functions finished

--- blast.py
+++ blast.py
@@ -1,28 +1,61 @@
 from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio import SeqIO
 import os
+from Bio.Blast import NCBIXML
 
-# Set custom db path
-# export BLASTDB="/home/gallegolab/Desktop/TFM/databases/BLAST/pdbaa"
+def run_blast_local(blastdb, db, fasta_dir, query, extension=".fa"):
+    """
+    Run BLAST Locally
+    """
 
-os.environ["BLASTDB"]="/home/gallegolab/Desktop/TFM/databases/BLAST/pdbaa"
+    # Set custom db path
+    os.environ["BLASTDB"]=blastdb
+    
+    # Call BLAST
+    blastp_cline = NcbiblastpCommandline(query=fasta_dir+query+extension, db=db, matrix="BLOSUM80",outfmt=5, out=query+"_blast.out")
+    stdout, stderr = blastp_cline()
 
-db="pdbaa"
-fasta_dir="./templates/FASTA/"
-fasta_dir="./input_fasta/"
-query="CREB"
-extension=".fa"
+    outblast = "The results are stored in "+query+"_blast.out"
+    return outblast
 
-with open(fasta_dir+query+extension, "r") as handle:
-    for record in SeqIO.parse(handle, "fasta"):
-        print(record.id)
-        print(record.seq)
 
-blastp_cline = NcbiblastpCommandline(query=fasta_dir+query+extension, db=db, matrix="BLOSUM80",outfmt=6, out=query+"_blast.out")
 
-stdout, stderr = blastp_cline()
 
-# Catch hits with evalue 0 and equal length as the query
-# Headers for the format 5 in BLAST:
-# qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
 
+def exact_match_retriever(filename):
+    """
+    Given a XML (format 5) formatted BLAST output, 
+    return a list of exact matches, meaning e-value == 0.0
+    AND length of the query == length of the match.
+    """
+
+    with open(filename) as result_handle:
+        blast_record = NCBIXML.read(result_handle)
+
+    # Get exact matches Eval+length
+    matches = []
+    E_VALUE_THRESH = 0.0
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect == E_VALUE_THRESH:
+                if alignment.length == len(hsp.query):
+                    ID = alignment.title[4:8]
+                    matches.append(ID)
+    return(matches)
+
+
+if __name__ == "__main__":
+    # Set vars for BLAST
+    blastdb = "/home/gallegolab/Desktop/TFM/databases/BLAST/pdbaa"
+    db="pdbaa"
+    fasta_dir="./templates/FASTA/"
+    fasta_dir="./input_fasta/"
+    query="test"
+    extension=".fa"
+
+    # Run BLAST
+    outblast =run_blast_local(blastdb, db, fasta_dir, query)
+    print(outblast)
+
+    matches = exact_match_retriever("test_blast.out")
+    print(matches)
